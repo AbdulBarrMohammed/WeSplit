881d12e95f30b35010d51eee5eba6fad975020cd
Abdul-Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-08-11 17:03:00 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Barr Mohammed on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeSplitTests/WeSplitTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitTests.swift
+//  WeSplitTests
+//
+//  Created by Barr Mohammed on 8/11/25.
+//
+
+import Testing
+@testable import WeSplit
+
+struct WeSplitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
